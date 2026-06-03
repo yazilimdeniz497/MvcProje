fd81c5ea04f66277ce47a7102e0817cc290e1cb8
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-03 08:35:54 +0300

    Contact düzenlendi

--- MvcProje/Controllers/HomeController.cs
+++ MvcProje/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace MvcProje.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Senin iletişim sayfan.";
 
             return View();
         }
